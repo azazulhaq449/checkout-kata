dcb70d76e0a766013e21bf7674d833255a62fa24
azaz449 <azaz.ulhaqq@gmail.com>
2023-07-13 17:06:40 +0100

    Initial commit

---
+++ CheckoutService/CheckoutService.Test/CheckoutServiceTest.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CheckoutService.Test
+{
+    [TestClass]
+    public class CheckoutServiceTest
+    {
+       
+    }
+}
---
+++ CheckoutService/CheckoutService.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CheckoutService.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CheckoutService.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0823c5bd-0b03-4b79-a7e6-513f051af82b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
